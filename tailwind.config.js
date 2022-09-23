8a54b5d3b3b2fcc66d67949baab3891d2380d7ad
zyzer01 <davidicfola@gmail.com>
2022-09-23 10:29:40 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#C4A78B',
+        danger: '#1B1A1B',
+        secondary: '#5E4634', 
+        success: '#CFC1A1',
+        brown: '#917457'
+      },
+    },
+  },
+  plugins: [],
+}
