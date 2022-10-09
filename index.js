50e0e5df14a223238cf7ff2e74fed72334937ad6
zyzer01 <davidicfola@gmail.com>
2022-10-09 11:01:22 +0100

    Add: Installed mongoDB to handle form and a success and failure page

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const ejs = require("ejs")
+const https = require("https")
+const mongoose = require("mongoose")
+const { Schema, model } = mongoose;
+const bodyParser = require("body-parser")
+const port = 8000
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/waitlistDB")
+
+const emailSchema = new Schema({
+    email: String
+});
+
+const Email = model('Email', emailSchema);
+
+app.set('view-engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", (req, res) => {
+    res.render('index.ejs')
+})
+
+
+app.post("/", (req, res) => {
+    const email = req.body.email;
+
+    var query = {email: email},
+    update = { expire: new Date() },
+    options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+    
+    Email.findOneAndUpdate(query, update, options, function(error, result) {
+        if (error) return;
+    });
+
+    if (res.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html")
+    } else {
+        res.sendFile(__dirname + "/failed.html")
+    }
+
+})
+
+
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`App is listening on port ${port}`);
+})
+
